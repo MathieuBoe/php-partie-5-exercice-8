bba2ed88d74066477236895e5c8c0b74303f3db2
MathieuBoe <mathieuboe60@gmail.com>
2020-06-08 11:22:31 +0200

    Exercice complet

---
+++ Exercice8.php
@@ -0,0 +1,12 @@
+<?php
+$n1 = 5;
+$n2 = 12;
+$n3 = 21;
+$title = 'Exercice8';
+
+include 'header.php';
+include '../Exercice1/Exercice1.php';
+foreach ($months as $value){
+    echo ' '.$value;
+}
+include 'footer.php';?>
